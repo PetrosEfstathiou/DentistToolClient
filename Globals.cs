d069a8a4807b7e6a892beaa4b39f8a38c322e97c
petro <petro@DESKTOP-UFU1TRV>
2022-06-09 13:43:44 +0300

    fixed gmails less secure application that got applied 30/5/2022

--- Globals.cs
+++ Globals.cs
@@ -12,7 +12,7 @@ namespace DentistToolClient
         public static string JWTtoken = "";
         public static int FinishedApp;
         public static string emailadd = "dentisttoolclient@gmail.com";
-        public static string epass = "69YFZPHYvF7GJEc";
+        public static string epass = "ravouudoegromrfy";
         public static string eusername = "DentistToolClient";
          
     }
